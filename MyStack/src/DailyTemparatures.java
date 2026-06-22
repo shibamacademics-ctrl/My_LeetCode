3782464a5fd8d32a5dc80b8d482410c24777d97d
shibamacademics-ctrl <shibamacademics@gmail.com>
2026-06-22 22:39:24 +0530

    Stack Problems

--- MyStack/src/DailyTemparatures.java
+++ MyStack/src/DailyTemparatures.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class DailyTemparatures {
     public static int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
+        /*int n = temperatures.length;
         int[] answer = new int[n];
 
         Stack<Integer> st = new Stack<>();
@@ -16,9 +16,29 @@ public class DailyTemparatures {
             st.push(i);
         }
 
-        return answer;
+        return answer;*/
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        Stack<Integer> st = new Stack<>(); // stores indices
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() &&
+                    temperatures[st.peek()] <= temperatures[i]) {
+                st.pop();
+            }
+
+            if (!st.isEmpty()) {
+                res[i] = st.peek() - i;
+            }
+
+            st.push(i);
+        }
+
+        return res;
     }
-    public static void main(String args[]){
+    public static void DailyTemparatures(String args[]){
         int temp[] = {73,74,75,71,69,72,76,73};
         System.out.println("Daily Temparatures list is: "+Arrays.toString(dailyTemperatures(temp)));
 
