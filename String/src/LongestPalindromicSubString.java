dbf641309b4fdeb52f4fbb15ef688f124a4fdfc3
shibamacademics-ctrl <shibamacademics@gmail.com>
2026-08-08 23:10:10 +0530

    String Problems

---
+++ String/src/LongestPalindromicSubString.java
@@ -0,0 +1,40 @@
+public class LongestPalindromicSubString {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() &&
+                s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad")); // bab or aba
+        System.out.println(longestPalindrome("cbbd"));  // bb
+        System.out.println(longestPalindrome("racecar")); // racecar
+    }
+}
+
